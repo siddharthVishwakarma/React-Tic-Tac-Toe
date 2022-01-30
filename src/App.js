c0ce80b0d0890cd1b6f0577e5bf87587a990cf91
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-01-30 14:10:23 +0530

    icon added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Icon from "./components/Icon";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +16,7 @@ function App() {
         >
           Learn React
         </a>
+        <Icon />
       </header>
     </div>
   );
